2b44e781f26758b7e6c323b15a94b9750f5e03c5
Daniel <danielazoulay2010@gmail.com>
2023-10-16 19:00:39 -0300

    exercicios 3

--- Javascript/exercicios/aula-03.js
+++ Javascript/exercicios/aula-03.js
@@ -1,11 +1,10 @@
-const frutas = [' banana', 'Maçã', 'Abacaxi', 'Pêra', 'Uva'];
+const frutas = prompt("Digite uma fruta: ");
+
+console.log(frutas);
 console.log(frutas.length);
 
-console.log(frutas[0].substring(0,3));
-
+console.log(frutas[0].substring(0, 3));
 
 console.log(frutas[0].trim());
 
-console.log(frutas.map(fruta=> {
-    return `fruta: ${fruta.toLowerCase()}`
-}))
+console.log(frutas.map(fruta => `fruta: ${fruta.toLowerCase()}`));
