ae04e2563858dacd86bcaad36a625c3fdb01d3f6
Daniel <danielazoulay2010@gmail.com>
2023-10-25 21:48:27 -0300

    aula de classes

---
+++ Javascript/aula-10/classes.js
@@ -0,0 +1,17 @@
+class Aluno {
+	constructor(ra, nome, idade) {
+		(this.ra = ra), (this.nome = nome), (this.idade = idade);
+	}
+}
+
+const AlunoDaniel = new Aluno(2323190, "Daniel", 18);
+
+
+console.log(AlunoDaniel);
+
+
+const teste = {
+    nome: "teste"
+}
+
+console.log(teste);
---
+++ Javascript/aula-10/praticando/class.js
@@ -0,0 +1,14 @@
+class Carro {
+	constructor(marca, modelo, valor, ano, cor) {
+		this.marca, this.modelo, this.valor, this.ano, this.cor;
+	}
+}
+
+const Cayenne = new Carro(
+	"Porsche",
+	"Cayenne",
+	"1.000.000.00",
+	"2023",
+	"Azul Royal"
+);
+const Macan = new Carro("Porsche", "Macan", "600.000", "2023", "White Moon");
