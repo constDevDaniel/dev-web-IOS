2e9b4165d952bcf2260d3b6a758f5dff6a6d6fd9
Daniel Dos Santos Azoulay <danielazoulay2010@gmail.com>
2023-10-28 16:38:22 -0300

    atividade 11

---
+++ Manipulando DOM/main.js
@@ -0,0 +1,19 @@
+// window.alert("Seja bem vindo ")
+document.title = "Atividade de DOM"
+
+const $section = document.createElement("section")
+const $article = document.createElement("article")
+const $h1_Section = document.createElement("h1")
+const $p_Section = document.createElement("p");
+const $h1_Article = document.createElement("h1");
+const $p_Article = document.createElement("p");
+
+
+document.body.appendChild($section)
+document.body.appendChild($article)
+$section.appendChild($h1_Section)
+$section.appendChild($p_Section)
+$article.appendChild($h1_Article)
+$article.appendChild($p_Article)
+
+
